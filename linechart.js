2982706f0f5b3c09c8da5c29b9e5e12474a5ebc7
Wan Zulmuhammad Harith <31917565+wzharith@users.noreply.github.com>
2021-03-08 17:32:40 +0800

    commment console log

--- linechart.js
+++ linechart.js
@@ -31,7 +31,7 @@ d3.csv("group.csv", function(error, data) {
       d = type(d);
   });
 
-  console.log(data);
+//   console.log(data);
 //   console.log(parse(data.date));
 
   // Filter to one symbol; the S&P 500.
@@ -48,9 +48,9 @@ d3.csv("group.csv", function(error, data) {
   y.domain([0, d3.max(msft, function(d) { return d.title; })]).nice();
 
 //   console.log(d3.max(values, function(d) { return d.title; }));
-  console.log(values[0].date);
-  console.log(x.domain());
-  console.log(y.domain());
+//   console.log(values[0].date);
+//   console.log(x.domain());
+//   console.log(y.domain());
 
   // Add an SVG element with the desired dimensions and margin.
   var svg = d3.select("#linechart")
@@ -95,7 +95,7 @@ d3.csv("group.csv", function(error, data) {
           return line(d);
         })
         .on("mousemove", function(d,i){
-          console.log(d[i])
+//           console.log(d[i])
           d3.select(this).style("stroke-width", 4);
           tooltip.style("visibility", "visible")
           .html(`<b>${d[i].type}</b>`)
